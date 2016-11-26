5512f963ad7bdd2d7690b73fa155d39d3cb28907
Neil Francis Ramirez Hipona <nferocious76@gmail.com>
2016-11-26 17:35:16 +0800

    Add clock hand and label

--- NFAnalogClock/NFAnalogClockView.h
+++ NFAnalogClock/NFAnalogClockView.h
@@ -10,13 +10,16 @@
 
 @interface NFAnalogClockView : UIView
 
+#pragma mark - Clock Design
+
 // hour
 @property (nonatomic, strong) UIColor *hourDialColor;
 @property (nonatomic, strong) UIColor *hourHandColor;
 
 @property (nonatomic) CGFloat hourDialWidth;
 @property (nonatomic) CGFloat hourDialLength;
-
+@property (nonatomic) CGFloat hourHandLength;
+@property (nonatomic) CGFloat hourHandWidth;
 
 // minutes
 @property (nonatomic, strong) UIColor *minDialColor;
@@ -24,7 +27,8 @@
 
 @property (nonatomic) CGFloat minDialWidth;
 @property (nonatomic) CGFloat minDialLength;
-
+@property (nonatomic) CGFloat minHandLength;
+@property (nonatomic) CGFloat minHandWidth;
 
 // seconds
 @property (nonatomic, strong) UIColor *secDialColor;
@@ -32,10 +36,19 @@
 
 @property (nonatomic) CGFloat secDialWidth;
 @property (nonatomic) CGFloat secDialLength;
+@property (nonatomic) CGFloat secHandLength;
+@property (nonatomic) CGFloat secHandWidth;
 
+#pragma mark - Clock Face
 
 // clock's radius
 @property (nonatomic) CGFloat radius;
 
+@property (nonatomic) BOOL enableMinDial;
+@property (nonatomic) BOOL enableSecDial;
+@property (nonatomic) BOOL enableClockLabel;
+
+@property (nonatomic, strong) UIFont *hourLabelFont;
+@property (nonatomic, strong) UIColor *hourLabelColor;
 
 @end
--- NFAnalogClock/NFAnalogClockView.m
+++ NFAnalogClock/NFAnalogClockView.m
@@ -20,14 +20,18 @@
 - (void)loadDefaultData {
     
     self.backgroundColor = [[UIColor lightGrayColor] colorWithAlphaComponent:0.5];
-    
+
+    // default radius
+    self.radius = self.frame.size.height > self.frame.size.width ? self.frame.size.width / 2 : self.frame.size.height / 2;
+
     // hour
     self.hourDialColor = [UIColor lightGrayColor];
     self.hourHandColor = [UIColor grayColor];
     
     self.hourDialWidth = 2;
     self.hourDialLength = 10;
-    
+    self.hourHandLength = self.radius * 0.7;
+    self.hourHandWidth = 3;
     
     // minutes
     self.minDialColor = [[UIColor greenColor] colorWithAlphaComponent:0.8];
@@ -35,18 +39,27 @@
     
     self.minDialWidth = 1.5;
     self.minDialLength = 5;
-    
+    self.minHandLength = self.radius * 0.9;
+    self.minHandWidth = 2;
+
     // seconds
     self.secDialColor = [[UIColor blueColor] colorWithAlphaComponent:0.6];
     self.secHandColor = [UIColor blueColor];
     
     self.secDialWidth = 1;
     self.secDialLength = 2.5;
+    self.secHandLength = self.radius * 0.9;
+    self.secHandWidth = 1;
+
+    self.enableMinDial = YES;
+    self.enableSecDial = YES;
+    self.enableClockLabel = YES;
     
-    // default radius
-    self.radius = self.frame.size.height > self.frame.size.width ? self.frame.size.width / 2 : self.frame.size.height / 2;
-    
+    self.hourLabelFont = [UIFont systemFontOfSize:12];
+    self.hourLabelColor = [UIColor blackColor];
 }
+
+#pragma mark - Drawing
 
 // Only override drawRect: if you perform custom drawing.
 // An empty implementation adversely affects performance during animation.
@@ -63,70 +76,174 @@
     
     // draw hour dial pins
     [self drawHourDialAtCenterPoint:canvasCenterPoint];
-    [self drawMinuteDialAtCenterPoint:canvasCenterPoint];
-    [self drawSecondDialAtCenterPoint:canvasCenterPoint];
+    
+    if (self.enableMinDial) {
+        [self drawMinuteDialAtCenterPoint:canvasCenterPoint];
+    }
+    
+    if (self.enableSecDial) {
+        [self drawSecondDialAtCenterPoint:canvasCenterPoint];
+    }
+    
+    if (self.enableClockLabel) {
+        [self drawHourLabelAtCenterPoint:canvasCenterPoint];
+    }
+    
+    // clock's hand
+    [self drawClockHourHand];
+    [self drawClockMinHand];
+    [self drawClockSecHand];
+}
+
+#pragma mark - Setters
+
+- (void)setEnableMinDial:(BOOL)enableMinDial {
+    _enableMinDial = enableMinDial;
+    
+    [self setNeedsDisplay];
+}
+
+- (void)setEnableSecDial:(BOOL)enableSecDial {
+    _enableSecDial = enableSecDial;
+    
+    [self setNeedsDisplay];
 }
 
 #pragma mark - Utils
 
+/** Clock Dials */
+
 - (void)drawHourDialAtCenterPoint:(CGPoint)center {
     
+    CGFloat angleCorrection = ToRadians(30 * 3); // angle correction start at angle 270°, default at 0°
     for (int i = 0; i < 12; i++) {
         
-        CGFloat angle = ToRadians(30 * i);
+        CGFloat angle = ToRadians(30 * i) - angleCorrection;
         CGPoint startPoint = PolarToDecart(center, self.radius - self.hourDialLength, angle);
         CGPoint endPoint = PolarToDecart(center, self.radius, angle);
         
         [self.hourDialColor setFill];
         [self.hourDialColor setStroke];
         
-        [self drawPinAtStartPoint:startPoint endPoint:endPoint dialWidth:self.hourDialWidth];
+        [self drawPinAtStartPoint:startPoint endPoint:endPoint width:self.hourDialWidth capStype:kCGLineCapSquare];
     }
 }
 
 - (void)drawMinuteDialAtCenterPoint:(CGPoint)center {
     
+    CGFloat angleCorrection = ToRadians(30 * 3); // angle correction start at angle 270°, default at 0°
     for (int i = 0; i < 60; i++) {
         if (i % 5 == 0) { continue; }
         
-        CGFloat angle = ToRadians(6 * i);
+        CGFloat angle = ToRadians(6 * i) - angleCorrection;
         CGPoint startPoint = PolarToDecart(center, self.radius - self.minDialLength, angle);
         CGPoint endPoint = PolarToDecart(center, self.radius, angle);
         
         [self.minDialColor setFill];
         [self.minDialColor setStroke];
         
-        [self drawPinAtStartPoint:startPoint endPoint:endPoint dialWidth:self.minDialWidth];
+        [self drawPinAtStartPoint:startPoint endPoint:endPoint width:self.minDialWidth capStype:kCGLineCapButt];
     }
 }
 
 - (void)drawSecondDialAtCenterPoint:(CGPoint)center {
     
+    CGFloat angleCorrection = ToRadians(30 * 3); // angle correction start at angle 270°, default at 0°
     for (int i = 0; i < 360; i++) {
         if (i % 6 == 0) { continue; }
 
-        CGFloat angle = ToRadians(1 * i);
+        CGFloat angle = ToRadians(1 * i) - angleCorrection;
         CGPoint startPoint = PolarToDecart(center, self.radius - self.secDialLength, angle);
         CGPoint endPoint = PolarToDecart(center, self.radius, angle);
         
         [self.secDialColor setFill];
         [self.secDialColor setStroke];
         
-        [self drawPinAtStartPoint:startPoint endPoint:endPoint dialWidth:self.secDialWidth];
+        [self drawPinAtStartPoint:startPoint endPoint:endPoint width:self.secDialWidth capStype:kCGLineCapRound];
     }
 }
 
-
-- (void)drawPinAtStartPoint:(CGPoint)startPoint endPoint:(CGPoint)endPoint dialWidth:(CGFloat)dialWidth {
+- (void)drawPinAtStartPoint:(CGPoint)startPoint endPoint:(CGPoint)endPoint width:(CGFloat)width capStype:(CGLineCap)capStype {
     
     UIBezierPath *path = [UIBezierPath bezierPath];
-    [path setLineWidth:dialWidth];
+    [path setLineWidth:width];
     [path moveToPoint:startPoint];
     [path addLineToPoint:endPoint];
     
-    [path setLineCapStyle:kCGLineCapSquare];
+    [path setLineCapStyle:capStype];
     [path fill];
     [path stroke];
 }
+
+/** Clock Label */
+
+- (void)drawHourLabelAtCenterPoint:(CGPoint)center {
+    
+    CGFloat angleCorrection = ToRadians(30 * 3); // angle correction start at angle 270°, default at 0°
+    for (int i = 1; i <= 12; i++) {
+        
+        CGFloat angle = ToRadians(30 * i) - angleCorrection;
+        CGFloat radiusPoint = self.radius - self.hourDialLength - 10;
+        CGPoint startPoint = PolarToDecart(center, radiusPoint, angle);
+        
+        NSString *hourLabel = [NSString stringWithFormat:@"%d", i];
+        NSDictionary *fontAttribute = @{NSFontAttributeName: self.hourLabelFont, NSForegroundColorAttributeName: self.hourLabelColor};
+        CGSize hourLabelSize = [hourLabel sizeWithAttributes:fontAttribute];
+        
+        CGFloat originX = startPoint.x - (hourLabelSize.width / 2);
+        CGFloat originY = startPoint.y - (hourLabelSize.height / 2);
+        CGRect hourLabelRect = CGRectMake(originX, originY, hourLabelSize.width, hourLabelSize.height);
+        
+        [hourLabel drawInRect:hourLabelRect withAttributes:fontAttribute];
+    }
+}
+
+/** Clock Hands */
+
+- (void)drawClockHourHand {
+    
+    // canvas center
+    CGPoint center = CGPointMake(self.frame.size.width / 2, self.frame.size.height / 2);
+
+    CGFloat angle = ToRadians(30 * 3);
+    CGPoint startPoint = PolarToDecart(center, -(self.hourHandLength * 0.15), angle);
+    CGPoint endPoint = PolarToDecart(center, self.hourHandLength, angle);
+
+    [self.hourHandColor setFill];
+    [self.hourHandColor setStroke];
+
+    [self drawPinAtStartPoint:startPoint endPoint:endPoint width:self.hourHandWidth capStype:kCGLineCapRound];
+}
+
+- (void)drawClockMinHand {
+    
+    // canvas center
+    CGPoint center = CGPointMake(self.frame.size.width / 2, self.frame.size.height / 2);
+    
+    CGFloat angle = ToRadians(60 * 3);
+    CGPoint startPoint = PolarToDecart(center, -(self.minHandLength * 0.25), angle);
+    CGPoint endPoint = PolarToDecart(center, self.minHandLength, angle);
+    
+    [self.minHandColor setFill];
+    [self.minHandColor setStroke];
+    
+    [self drawPinAtStartPoint:startPoint endPoint:endPoint width:self.minHandWidth capStype:kCGLineCapRound];
+}
+
+- (void)drawClockSecHand {
+    
+    // canvas center
+    CGPoint center = CGPointMake(self.frame.size.width / 2, self.frame.size.height / 2);
+    
+    CGFloat angle = ToRadians(60 * 1);
+    CGPoint startPoint = PolarToDecart(center, -(self.secHandLength * 0.3), angle);
+    CGPoint endPoint = PolarToDecart(center, self.secHandLength, angle);
+    
+    [self.secHandColor setFill];
+    [self.secHandColor setStroke];
+    
+    [self drawPinAtStartPoint:startPoint endPoint:endPoint width:self.secHandWidth capStype:kCGLineCapRound];
+}
+
 
 @end
